83c6981bbd57ae508ee6fdbda663b1fa311e34dd
mvsantosdev <mvsantos@protonmail.com>
2020-09-12 12:26:39 -0300

    upgrade Survey __init__

--- frblip/survey.py
+++ frblip/survey.py
@@ -1,3 +1,7 @@
+import os
+
+import json
+
 import numpy
 
 import pandas
@@ -19,13 +23,10 @@ class Survey():
     """
     Class which defines a Radio Survey
     """
-
-    def __init__(self, name='bingo', file=None, kind='gaussian',
-                 az0=0.0, alt0=0.0, polarizations=2,
-                 frequency_bands=[1000.0, 1300.0],
-                 system_temperature=70.0, sampling_time=1.0,
-                 degradation_factor=1.0, start_time=None):
-
+    
+    def __init__(self, name='bingo', kind='gaussian', start_time=None,
+                 **kwargs):
+        
         """
         Creates a Survey object.
 
@@ -60,113 +61,42 @@ class Survey():
         """
 
         if start_time is None:
+            
             self.start_time = Time.now()
-
-        self.name = name
-
-        self.start_time = start_time
-
-        self.degradation_factor = degradation_factor
-        self.system_temperature = system_temperature * units.K
-        self.frequency_bands = numpy.array(frequency_bands).reshape(-1, 1)
-        self.frequency_bands = self.frequency_bands * units.MHz
-        self.band_widths = numpy.diff(self.frequency_bands, axis=0)
-        self.sampling_time = sampling_time * units.ms
-        self.polarizations = polarizations
-        
-        beams = None
-        
-        if file is None:
-        
-            beams = numpy.load('{}/{}.npz'.format(_DATA, name))
             
         else:
             
-            beams = numpy.load(file)
-
-        self.location = coordinates.EarthLocation(
-            lat=coordinates.Angle(beams['Latitude (degree)'], unit=units.deg),
-            lon=coordinates.Angle(beams['Longitude (degree)'], unit=units.deg),
-            height=beams['Elevation (meter)'] * units.m
-        )
-
-        self.az = (beams['Azimuth (degree)'] + az0) * units.degree
-        self.alt = (beams['Altitude (degree)'] + alt0) * units.degree
-        self.solid_angle = beams['Solid Angle (degree^2)'] * units.degree**2
-        self.gain = beams['Gain (K/Jy)'] * units.K / units.Jy
-
-        self.nbeams = len(self.gain)
-
-        self.columns = [
-            '{}_{}'.format(self.name, i)
-            for i in range(self.nbeams)
-        ]
-
-        sa_rad = self.solid_angle.to(units.sr).value
-
-        radius = numpy.arccos(1 - sa_rad / (2 * numpy.pi)) * units.rad
-
-        self.radius = radius.to(units.degree)
+            self.start_time = start_time
+            
+        name_ = '{}/{}.npz'.format(_DATA, name)
+            
+        if os.path.exists(name):
+            
+            input_dict = self._load_from_file(name)
+            
+        elif os.path.exists(name_):
+            
+            input_dict = self._load_from_file(name_)
+            
+        else:
+            
+            input_dict = kwargs
+            
+        self._load_params(**input_dict)
+        self._load_beams(**input_dict)
 
         if kind == 'grid':
 
-            self.pattern = beams['Pattern']
-
-            az_min, az_max, naz = beams['Azimuth Range (degree)']
-            alt_min, alt_max, nalt = beams['Altitude Range (degree)']
-
-            self.az_min, self.az_max = az_min, az_max
-            self.alt_min, self.alt_max = alt_min, alt_max
-
-            self.naz = int(naz)
-            self.nalt = int(nalt)
-
-            self.az_grid = numpy.linspace(az_min, az_max, self.naz)
-            self.alt_grid = numpy.linspace(alt_min, alt_max, self.nalt)
-
-            self.az_grid += az0
-            self.alt_grid += alt0
-
-            self.Fs = [
-                numpy.vectorize(
-                    RectBivariateSpline(self.az_grid, self.alt_grid, P),
-                    otypes=[numpy.float]
-                )
-                for P in self.pattern
-            ]
-
-            self.selection = self._grid
+            self._load_grid(**input_dict)
 
         elif kind in ('tophat', 'bessel', 'gaussian'):
-
-            self.pattern = patterns[kind]
-
-            def selection(az, alt):
-
-                arcs = angular_separation(az, alt, self.az, self.alt)
-
-                rescaled_arc = (arcs / self.radius).to(1).value
-
-                return self.pattern(rescaled_arc)
-
-            self.selection = selection
+            
+            self._set_selection(kind)
 
         else:
 
             print('Please choose a valid pattern kind')
-
-        scaled_time = self.band_widths * self.sampling_time
-        w = numpy.sqrt(self.polarizations * scaled_time)
-
-        self.minimum_temperature = (self.system_temperature / w).to(units.K)
-
-    def _grid(self, az, alt):
-
-        return numpy.row_stack([
-            F(az.value, alt.value)
-            for F in self.Fs
-        ])
-
+            
     def __call__(self, frb):
 
         coords = frb.get_local_coordinates(self.location)
@@ -179,4 +109,164 @@ class Survey():
 
         signal = response[:, :, numpy.newaxis] * Speak
 
-        return coords, signal.swapaxes(0, 1)
+        return coords, signal
+            
+    def _load_params(self, frequency_bands, polarizations,
+                     system_temperature, sampling_time,
+                     longitude, latitude, elevation,
+                     degradation_factor, **kwargs):
+        
+        """
+        This is a private function, please do not call it
+        directly unless you know exactly what you are doing.
+        """
+        
+        self.degradation_factor = degradation_factor
+        self.system_temperature = system_temperature * units.K
+        self.frequency_bands = frequency_bands.reshape(-1, 1)
+        self.frequency_bands = self.frequency_bands * units.MHz
+        self.band_widths = numpy.diff(self.frequency_bands, axis=0)
+        self.sampling_time = sampling_time * units.ms
+        self.polarizations = polarizations
+        
+        lon = coordinates.Angle(longitude, unit=units.degree)
+        lat = coordinates.Angle(latitude, unit=units.degree)
+        el = elevation * units.meter
+        
+        self.location = coordinates.EarthLocation(
+            lon=lon, lat=lat,
+            height=elevation
+        )
+        
+        scaled_time = (self.band_widths * self.sampling_time).to(1)
+        noise_scale = numpy.sqrt(polarizations * scaled_time.value)
+
+        self.minimum_temperature = self.system_temperature / noise_scale
+        
+    def _load_beams(self, az, alt, solid_angle, gain, **kwargs):
+        
+        """
+        This is a private function, please do not call it
+        directly unless you know exactly what you are doing.
+        """
+        
+        self.az = az * units.degree
+        self.alt = alt * units.degree
+        self.solid_angle = solid_angle * units.degree**2
+        self.gain = gain * units.K / units.Jy
+
+        self.nbeams = len(gain)
+
+        sa_rad = self.solid_angle.to(units.sr).value
+        
+        arg = 1 - sa_rad / (2 * numpy.pi)
+
+        radius = numpy.arccos(arg) * units.rad
+
+        self.radius = radius.to(units.degree)
+    
+    def _load_grid(self, pattern, az_range, alt_range, **kwargs):
+        
+        """
+        This is a private function, please do not call it
+        directly unless you know exactly what you are doing.
+        """
+        
+        self.pattern = pattern
+
+        az_min, az_max, naz = az_range
+        alt_min, alt_max, nalt = alt_range
+
+        self.az_min, self.az_max = az_min, az_max
+        self.alt_min, self.alt_max = alt_min, alt_max
+
+        self.naz = int(naz)
+        self.nalt = int(nalt)
+
+        self.az_grid = numpy.linspace(az_min, az_max, self.naz)
+        
+        self.alt_grid = numpy.linspace(alt_min, alt_max, self.nalt)
+
+        self.Fs = [
+            numpy.vectorize(
+                RectBivariateSpline(
+                    self.az_grid, self.alt_grid, P
+                ),
+                otypes=[numpy.float]
+            )
+            for P in self.pattern
+        ]
+
+        self.selection = self._grid
+    
+    def _load_from_file(self, file):
+        
+        """
+        This is a private function, please do not call it
+        directly unless you know exactly what you are doing.
+        """
+        
+        try:
+            
+            filename, extension = os.path.splitext(file)
+            
+            if extension == '.npz': 
+        
+                data = numpy.load(file)
+            
+            elif extension == '.json':
+                
+                data = json.loads(file)
+                
+        except FileNotFoundError:
+            
+            print('File does not exist')
+            
+        return dict(
+            polarizations=int(data.get('Polarizations')),
+            system_temperature=float(data.get('System Temperature (K)')),
+            sampling_time=float(data.get('Sampling Time (ms)')),
+            longitude=str(data.get('Longitude (degree)')),
+            latitude=str(data.get('Latitude (degree)')),
+            elevation=float(data.get('Elevation (meter)')),
+            degradation_factor=float(data.get('Degradation Factor')),
+            frequency_bands=numpy.array(data.get('Frequency Bands (MHz)')),
+            pattern=numpy.array(data.get('Pattern')),
+            az=numpy.array(data.get('Azimuth (degree)')),
+            alt=numpy.array(data.get('Altitude (degree)')),
+            solid_angle=numpy.array(data.get('Solid Angle (degree^2)')),
+            gain=numpy.array(data.get('Gain (K/Jy)')),
+            az_range=numpy.array(data.get('Azimuth Range (degree)')),
+            alt_range=numpy.array(data.get('Altitude Range (degree)')),
+        )
+
+    def _set_selection(self, kind):
+        
+        """
+        This is a private function, please do not call it
+        directly unless you know exactly what you are doing.
+        """
+    
+        self.pattern = patterns[kind]
+
+        def selection(az, alt):
+
+            arcs = angular_separation(az, alt, self.az, self.alt)
+
+            rescaled_arc = (arcs / self.radius).to(1).value
+
+            return self.pattern(rescaled_arc)
+
+        self.selection = selection
+
+    def _grid(self, az, alt):
+        
+        """
+        This is a private function, please do not call it
+        directly unless you know exactly what you are doing.
+        """
+
+        return numpy.row_stack([
+            F(az.value, alt.value)
+            for F in self.Fs
+        ])
