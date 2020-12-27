b6adbe07361fccaf91507325c38a8f1a5ac97c79
Marcelo <mvsantos@protonmail.com>
2020-12-27 16:54:26 -0300

    Fix start time observation

--- frblip/radio_telescope.py
+++ frblip/radio_telescope.py
@@ -108,18 +108,19 @@ class RadioTelescope():
 
             print('Please choose a valid pattern kind')
 
-    def __call__(self, frb, coordinates=None, eps=-1):
+    def __call__(self, frb, coordinates=None, start_time=None, eps=-1):
+
+        if start_time is not None:
+
+            self.start_time = start_time
 
         if coordinates is None:
 
-            coords = frb.get_local_coordinates(self.location)
+            coordinates = frb.get_local_coordinates(self.location,
+                                                    start_time=self.start_time)
 
-        else:
-
-            coords = coordinates
-
-        az = coords.az
-        alt = coords.alt
+        az = coordinates.az
+        alt = coordinates.alt
 
         response = self.selection(az, alt)
 
@@ -141,7 +142,7 @@ class RadioTelescope():
                              time_factor.value.ravel(),
                              noise.value,
                              self.frequency_bands.value,
-                             coords,
+                             coordinates,
                              self.location)
 
         if eps > 0.0:
